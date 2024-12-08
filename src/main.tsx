e164248dcbff36df51d07852647419ba89582245
Avik-creator <avikm744@gmail.com>
2024-12-08 17:30:24 +0530

    Updated main.tsx.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {BrowserRouter} from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import { QueryClientProvider, QueryClient } from 'react-query'
+
+const queryClient = new QueryClient({defaultOptions: {
+  queries: {
+    refetchOnWindowFocus: false,
+  },
+}})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+   </BrowserRouter>
   </StrictMode>,
 )
