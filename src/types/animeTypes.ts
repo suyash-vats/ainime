6680461aee454a70a18535c7b8c03a187e3b2188
Avik-creator <avikm744@gmail.com>
2024-12-08 17:24:17 +0530

    Added Types for Animes and Items.

---
+++ src/types/animeTypes.ts
@@ -0,0 +1,26 @@
+type EpisodeType = {
+    id: string
+    number: number
+    url: string
+}
+
+export type animeDataType = {
+    id: string
+    title: string
+    url: string
+    genres: string
+    totalEpisodes: number
+    image: string
+    releaseDate: string
+    description: string
+    type: string
+    status: string
+    otherName: string
+    subOrDub: string
+    episodes: EpisodeType[]
+}
+
+export type episodeDataType = {
+    name: string
+    url: string
+}[]
---
+++ src/types/itemTypes.ts
@@ -0,0 +1,15 @@
+export type ItemType = {
+    id: string
+    title: string
+    image: string
+    genres: string[]
+    episodeNumber: number
+    releaseDate: string
+}
+
+export type ItemListType = {
+    animeId: string
+    animeName: string
+    animeImage: string
+    totalEpisodes: number
+}
