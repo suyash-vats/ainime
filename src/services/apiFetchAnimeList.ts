75ab97d5735610d6b267253600c2db0ba6d0de98
Avik-creator <avikm744@gmail.com>
2024-12-08 17:04:30 +0530

    Added Fetch API Capabilities.

---
+++ src/services/apiFetchAnimeList.ts
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+export const getAnimeList = async (category: string, page?: number) => {
+    try{
+        const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/anime/gogoanime/${category}`, {
+            params: {
+              page: page,
+            },
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          })
+        return response.data;
+    }catch(error: unknown | string){
+        if(typeof error === 'string'){
+            throw new Error(error);
+        }else if (error instanceof Error){
+            throw new Error(error.message);
+        }else{
+            throw new Error('Error fetching data');
+        }
+    }
+}
+
+
+export const getAnime = async (id : string) => {
+    try { 
+      const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/anime/gogoanime/info/${id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      return response.data
+    } catch (error : unknown | string) {
+      if (typeof error === 'string') {
+        throw new Error(error)
+      } else if (error instanceof Error) {
+        throw error
+      } else {
+        throw new Error('An unknown error occurred.');
+      }
+    }
+  }
+
+  export const getAnimeEpisode = async (episodeId : string) => {
+    try {
+      const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/anime/gogoanime/servers/${episodeId}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      return response.data
+    } catch (error : unknown | string) {
+      if (typeof error === 'string') {
+        throw new Error(error)
+      } else if (error instanceof Error) {
+        throw error
+      } else {
+        throw new Error('An unknown error occurred.');
+      }
+    }
+  }
