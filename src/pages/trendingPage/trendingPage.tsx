dd900c2e4e8186398af15701cd2dea0bab236e0c
Avik-creator <avikm744@gmail.com>
2024-12-10 00:30:09 +0530

    Added Final App.tsx

--- src/pages/trendingPage/trendingPage.tsx
+++ src/pages/trendingPage/trendingPage.tsx
@@ -2,7 +2,7 @@ import { AnimeListContainer } from "../../components/AnimeList/AnimeListContaine
 import { Footer } from "../../components/Footer/Footer";
 import { Navbar } from "../../components/Navbar/Navbar";
 
-export const BookMarks = () => {
+export const TrendingPage = () => {
 
     return (
       <>
